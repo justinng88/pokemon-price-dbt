6b4de04648d1b88106c5ab7733d448377aaa9c79
Justin Ng <your@email.com>
2026-09-09 15:06:40 -0700

    Add intermediate layer: int_card_printings and int_price_snapshots

---
+++ analytics/models/intermediate/int_card_printings.sql
@@ -0,0 +1,42 @@
+﻿{{ config(materialized='view') }}
+
+with printings as (
+    select distinct
+        card_printing_key,
+        product_id,
+        printing_type
+    from {{ ref('stg_tcgcsv_prices') }}
+),
+
+products as (
+    select * from {{ ref('stg_tcgcsv_products') }}
+),
+
+sets as (
+    select * from {{ ref('stg_tcgcsv_groups') }}
+)
+
+select
+    printings.card_printing_key,
+    printings.product_id,
+    printings.printing_type,
+    products.product_name,
+    products.product_name_clean,
+    products.card_number,
+    products.rarity,
+    products.card_type,
+    products.evolution_stage,
+    products.hp,
+    products.is_sealed_product,
+    products.tcgplayer_url,
+    products.image_url,
+    sets.set_id,
+    sets.set_name,
+    sets.set_abbreviation,
+    sets.set_published_date,
+    sets.is_supplemental
+from printings
+inner join products
+    on printings.product_id = products.product_id
+left join sets
+    on products.group_id = sets.set_id
---
+++ analytics/models/intermediate/int_price_snapshots.sql
@@ -0,0 +1,41 @@
+﻿{{ config(materialized='view') }}
+
+with prices as (
+
+    select * from {{ ref('stg_tcgcsv_prices') }}
+
+),
+
+printings as (
+
+    select
+        card_printing_key,
+        is_sealed_product,
+        set_published_date
+    from {{ ref('int_card_printings') }}
+
+)
+
+select
+    prices.card_printing_key,
+    prices.price_date,
+    prices.product_id,
+    prices.printing_type,
+
+    prices.low_price,
+    prices.mid_price,
+    prices.high_price,
+    prices.market_price,
+    prices.direct_low_price,
+
+    printings.is_sealed_product,
+
+    date_diff('day', printings.set_published_date, prices.price_date)
+        as days_since_set_release,
+
+    prices.market_price is not null as has_market_price
+
+from prices
+
+inner join printings
+    on prices.card_printing_key = printings.card_printing_key
